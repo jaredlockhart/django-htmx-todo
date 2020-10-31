c526ecc9f2ce711832c323803230f7b6bc918ecf
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-10-31 19:59:17 -0400

    Use postgres

--- app/todo/settings.py
+++ app/todo/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRETKEY"]
+SECRET_KEY = "SECRETKEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: List[str] = [os.environ["HOSTNAME"]]
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "todo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
